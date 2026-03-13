57666ef9660eca495d52a0b98e3950e2a49b9384
theirgames <theirgames52@gmail.com>
2026-03-13 12:09:26 +0300

    Addressing review feedback

--- addons/godot_material_footsteps/scripts/material_detectors/terrain3d_material_detector.gd
+++ addons/godot_material_footsteps/scripts/material_detectors/terrain3d_material_detector.gd
@@ -1,6 +1,6 @@
 extends "./material_detector.gd"
 
-var terrain3d :Terrain3D
+var terrain3d
 var tex_id :Vector3
 var tex_id_string :String
 
@@ -9,6 +9,12 @@ func _init(terrain3d_ref) -> void:
 
 
 func detect(raycast: RayCast3D) -> Variant:
+	
+	if raycast == null:
+		return null
+	if terrain3d == null:
+		return null
+	
 	if raycast.get_collider():
 		var tex_id = terrain3d.data.get_texture_id(raycast.get_collision_point())
 		tex_id_string = str(tex_id.x)
